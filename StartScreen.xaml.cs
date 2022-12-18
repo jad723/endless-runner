44f4056c2c6cc3d42b9a6984c4708ad6f34b3002
jad723 <jadmaalouf2003.jm15@gmail.com>
2022-12-18 15:32:09 +0200

    Added Start Screen

---
+++ StartScreen.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Endless_Runner
+{
+    /// <summary>
+    /// Interaction logic for StartScreen.xaml
+    /// </summary>
+    public partial class StartScreen : Window
+    {
+        public StartScreen()
+        {
+            InitializeComponent();
+
+            MyCanvas.Focus();
+
+            ImageBrush backgroundSprite = new ImageBrush();
+
+            backgroundSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/background.gif"));
+
+            background.Fill = backgroundSprite;
+        }
+
+        private void LoadGame(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Hide();
+            window.Closed += Close;
+        }
+
+
+        private void LoadHelp(object sender, RoutedEventArgs e)
+        {
+            Help window = new Help();
+            window.Show();
+            this.Hide();
+            window.Closed += Close;
+        }
+
+        private void Close(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void Quit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
