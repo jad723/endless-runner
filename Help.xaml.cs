83a9b95ab0195ba86101ffcb7332e6db7d4c44bb
jad723 <jadmaalouf2003.jm15@gmail.com>
2022-12-18 15:32:43 +0200

    Made Help screen

---
+++ Help.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Endless_Runner
+{
+    /// <summary>
+    /// Interaction logic for Help.xaml
+    /// </summary>
+    public partial class Help : Window
+    {
+        public Help()
+        {
+            InitializeComponent();
+
+            MyCanvas.Focus();
+
+            ImageBrush backgroundSprite = new ImageBrush();
+
+            backgroundSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/background.gif"));
+
+            background.Fill = backgroundSprite;
+        }
+    }
+}
